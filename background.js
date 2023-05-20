a0765f16791a48de0968751ad941a1cd10442ae2
Nathan Machane <73017570+turtleship69@users.noreply.github.com>
2023-05-20 11:59:14 +0100

    Update to manifest v3 (that was kinda easy???)

--- background.js
+++ background.js
@@ -1,47 +1,42 @@
-function moveTabs(tabs, delay) {
+async function moveTabs(tabs, delay) {
     if (tabs.length === 0) {
         setTimeout(function() {
             closeWindows(tabs, delay);
         }, 3000);
         return;
     }
-    chrome.windows.create({tabId: tabs[0].id, state: "maximized", focused: true}, function() {
-        setTimeout(function() {
-            moveTabs(tabs.slice(1), delay);
-        }, 100);
-    });
+    await chrome.windows.create({tabId: tabs[0].id, state: "maximized", focused: true});
+    setTimeout(function() {
+        moveTabs(tabs.slice(1), delay);
+    }, 100);
 }
 
-function closeWindows(tabs, delay) {
-    chrome.windows.getAll({}, function(windows) {
-        windows.sort(function(a, b) {
-            return a.top - b.top;
-        });
-        let closeWindow = function(i) {
-            if (i >= windows.length) return;
-            chrome.windows.update(windows[i].id, {focused: true}, function() {
-                setTimeout(function() {
-                    chrome.windows.remove(windows[i].id, function() {
-                        setTimeout(function() {
-                            closeWindow(i + 1);
-                        }, delay);
-                    });
-                }, delay);
-            });
-        };
-        closeWindow(0);
+async function closeWindows(tabs, delay) {
+    let windows = await chrome.windows.getAll({});
+    windows.sort(function(a, b) {
+        return a.top - b.top;
     });
+    let closeWindow = async function(i) {
+        if (i >= windows.length) return;
+        await chrome.windows.update(windows[i].id, {focused: true});
+        setTimeout(async function() {
+            await chrome.windows.remove(windows[i].id);
+            setTimeout(function() {
+                closeWindow(i + 1);
+            }, delay);
+        }, delay);
+    };
+    closeWindow(0);
 }
 
-chrome.runtime.onMessage.addListener(function(message) {
+chrome.runtime.onMessage.addListener(async function(message) {
     if (message.moveTabs) {
         let delay = message.delay || 1000;
-        chrome.windows.getAll({populate: true}, function(windows) {
-            let tabs = [];
-            windows.forEach(function(window) {
-                tabs = tabs.concat(window.tabs);
-            });
-            moveTabs(tabs, delay);
+        let windows = await chrome.windows.getAll({populate: true});
+        let tabs = [];
+        windows.forEach(function(window) {
+            tabs = tabs.concat(window.tabs);
         });
+        moveTabs(tabs, delay);
     }
 });
